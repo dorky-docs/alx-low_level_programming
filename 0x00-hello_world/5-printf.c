d817a0d0e14a6f6739ea3b3b32132c0aed4d3de3
Dorcas Fordjour <docfords2017@gmail.com>
2023-06-15 21:39:57 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ *Return: always 0 (success)
+ */
+
+int main (void)
+{
+  printf("\"with proper grammar, but the outcome is a place of art");
+
+    return(0);
+}
