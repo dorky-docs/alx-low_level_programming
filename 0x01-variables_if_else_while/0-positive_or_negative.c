e5bf9ca1cb041aa688d7899e0ae345dbbfdb3ca7
Dorcas Fordjour <docfords2017@gmail.com>
2023-06-18 12:52:47 -0700

    Positive anythin gis better than negative nothing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,8 +7,8 @@
  *
  *Return: Always 0 (Success)
  */
-
 int main(void)
+
 {
 int n;
 
@@ -17,14 +17,14 @@ n = rand() - RAND_MAX / 2;
 
 if (n > 0)
 {
-  printf("%d is positive\n", n);
+printf("%d is positive\n", n);
 }
 else if(n == 0)
 {
-  printf("%d is zero\n", n);
+printf("%d is zero\n", n);
 else
 {
-  printf("%d is negative\n", n);
+printf("%d is negative\n", n);
 }
 return (0);
 }
