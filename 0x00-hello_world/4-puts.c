2e1d8416b5d6d6a43af3378e0955d4f4785e1e78
Dorcas Fordjour <docfords2017@gmail.com>
2023-06-18 07:36:49 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
+
 /**
 * main - Entry point
 *
 *Return: Always 0 (success)
 */
+
 int main(void)
+
 {
-    puts("\"Programming is like building a multilingual puzzle");
-  returm (0);
+    puts("Programming is like building a multilingual puzzle");
+  return (0);
 }
