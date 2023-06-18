eb3ca4f258858850b9a6e1dc6a9561abfbc938f8
Dorcas Fordjour <docfords2017@gmail.com>
2023-06-18 11:20:36 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -9,6 +9,6 @@
 
 int main(void)
 {
-printf("\"with proper grammar, but the outcome is a place of art,");
-return(0);
+printf("with proper grammar, but the outcome is a place of art,\n");
+return (0);
 }
