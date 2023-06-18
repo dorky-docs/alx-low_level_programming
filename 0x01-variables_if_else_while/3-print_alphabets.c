87f4e3fa06f08f7b2b2f31301d48ed4bee1b26ea
Dorcas Fordjour <docfords2017@gmail.com>
2023-06-18 06:31:58 -0700

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,15 +9,19 @@
 int main(void)
 
 {
-  char alp[52] =
+  char c;
+  char d;
+  
+  for (c = 'a'; c <= 'z'; c++)
 
-    ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z');
+    {
+      putchar(c);
+    }
 
-  int i;
+  for (d = 'A'; d <= 'Z'; d++)
+    {
+      putchar(d);
 
-  for(i = 0; i < 52; i++)
-
-    {putchar(alp[i]);
     }
   
   putchar('\n');
