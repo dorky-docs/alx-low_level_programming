f3732540456f592016a44d6148bed7d50e85d77c
Dorcas Fordjour <docfords2017@gmail.com>
2023-06-20 22:02:15 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,12 +2,12 @@
 
 /**
 *print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
-*follwed by a new line
+*followed by a new line
 */
 
 void print_alphabet_x10(void)
 {
-char ch
+char ch;
 int i;
 
 i = 0;
