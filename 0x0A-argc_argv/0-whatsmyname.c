869402bf748ce8317c3d3f6ff66480b29a1afd0a
Dorcas Fordjour <docfords2017@gmail.com>
2023-07-09 07:51:29 -0700

    That is all the code for this particular project on arguments in C-programming. See ya in the next one

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,10 +6,11 @@
  * @argc: number of arguments
  * @argv: array of arguments
  *
- *Return: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf("%s/n", *argv);
+	printf("%s\n", *argv);
+
 	return (0);
 }
