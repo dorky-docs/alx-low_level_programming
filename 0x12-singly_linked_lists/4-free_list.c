68d9c70f0d35e04119b8d7b10b9e859856cf85d6
Dorcas Fordjour <docfords2017@gmail.com>
2023-07-27 13:13:57 -0700

    We have just taken a stab at singly linked lists. There is still a wealth of knowledge to be had and i am here for it. We may be dne with this task, but i know we will take a deeper dive into this very soon.

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_list - this function frees a linked list
+ * @head: the list_t list to be freed
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+}
---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,28 @@
+#ifndef LIST_H
+#define LIST_H
+#include <stddef.h>
+
+/**
+ * struct list_s - a singly linked list
+ * @str: malloc'ed string
+ * @len: the length of the string
+ * @next: this is a pointer to the next node
+ * Description: this is a singly linked list node structure
+ * for Holberton project
+ */
+
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+int _putchar(char x);
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+
+#endif
